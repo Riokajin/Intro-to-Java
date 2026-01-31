17c58c49bf236f23a8c37836687f438228c7dfd4
Adam Evans <46998078+Riokajin@users.noreply.github.com>
2026-01-31 17:51:59 -0500

    initial book fields

--- Assignments/assignment-1/Book.java
+++ Assignments/assignment-1/Book.java
@@ -1,3 +1,9 @@
 public class Book {
     
+    // Fields to store book information
+    private String title;
+    private String author;
+    private String isbn;
+    private boolean available;
+
 }
