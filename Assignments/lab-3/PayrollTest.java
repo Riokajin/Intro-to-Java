33345359a5c46d0b4ee7e101097eb8deab888453
Adam Evans <46998078+Riokajin@users.noreply.github.com>
2026-02-05 17:11:46 -0500

    completed payrollTest todos and the lab

--- Assignments/lab-3/PayrollTest.java
+++ Assignments/lab-3/PayrollTest.java
@@ -3,10 +3,13 @@ public class PayrollTest {
     public static void main(String[] args) {
  
         // TODO 9: Create a CommissionEmployee object
+        CommissionEmployee employee = new CommissionEmployee("Adam", "Evans", "123-456-789", 5000.0, 0.10);
  
         // TODO 10: Display employee details
+        System.out.println(employee);
  
         // TODO 11: Display earnings
+        System.out.println("Earnings: " + employee.earnings());
     }
 }
  
