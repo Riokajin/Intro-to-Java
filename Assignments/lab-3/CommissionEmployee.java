fcaf8ca7dffeb9245f5e04977ca4419c3117713d
Adam Evans <46998078+Riokajin@users.noreply.github.com>
2026-02-05 14:25:23 -0500

    finished getters and setters with validation

--- Assignments/lab-3/CommissionEmployee.java
+++ Assignments/lab-3/CommissionEmployee.java
@@ -4,8 +4,32 @@ public class CommissionEmployee extends Employee {
     private double commissionRate;
  
     // TODO 5: Create constructor using super()
+    public CommissionEmployee(String firstName, String lastName, String socialSecurityNumber, double grossSales, double commissionRate){
+        super(firstName, lastName, socialSecurityNumber);
+        this.grossSales = grossSales;
+        this.commissionRate = commissionRate;
+    }
  
     // TODO 6: Create getters and setters with validation
+    public double getGrossSales() { 
+        return grossSales; 
+    } 
+    public double getCommissionRate() { 
+        return commissionRate; 
+    }
+public void setGrossSales(double grossSales) { 
+    if (grossSales < 0.0) {
+        throw new IllegalArgumentException("Sales must be a positive number");
+    } 
+    this.grossSales = grossSales;
+} 
+public void setCommissionRate(double commissionRate) { 
+    if (commissionRate <= 0.0 || commissionRate >= 1.0) {
+        throw new IllegalArgumentException("Commission Rate must be between 0.0 and 1.0");
+    }
+    this.commissionRate = commissionRate;
+}
+
  
     // TODO 7: Override earnings()
  
